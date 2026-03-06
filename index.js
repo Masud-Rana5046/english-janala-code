e1f63dd3dbfa933d62e900250e24867a87760b38
Masud Rana <masud@MacBook-Pro.local>
2026-03-06 21:48:54 +0600

    added fix some empty issues

--- index.js
+++ index.js
@@ -19,15 +19,28 @@ const displayLavelWord = (words) =>{
 const wordContainer = document.getElementById("word-container");
 wordContainer.innerHTML = "";
 
+if(words.length == 0){
+   // alert('no words detected')
+   wordContainer.innerHTML = `
+    <div class="text-center col-span-full">
+         <img class= "mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="text-xl font-medium text-gray-300 rounded-xl py-5 space-y-6">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+   `;
+    return;
+}
+
 words.forEach(word => {
     console.log(word);
 
     const card = document.createElement('div');
     card.innerHTML = `
      <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-5">
-            <h2 class="font-bold text-xl">${word.word}</h2>
+            <h2 class="font-bold text-xl">${word.word ? word.word :"poa jaini"}</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
-            <div class="font-semibold text-xl font-bangla">"${word.meaning}/ ${word.pronunciation}"</div>
+            <div class="font-semibold text-xl font-bangla">
+            "${word.meaning ? word.meaning : "poajaini"}/ ${word.pronunciation ? word.pronunciation: "poa jainigit"}"</div>
             <div class="flex justify-between items-center">
                 <button class=" btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                  <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-xmark"></i></button>
